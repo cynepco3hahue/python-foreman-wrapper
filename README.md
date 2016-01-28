3a376044b53951db665ce06040367ebf0d6c3d67
Lukianov Artyom <alukiano@redhat.com>
2016-01-28 14:21:35 +0200

    Add setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # python-foreman-wrapper
-Wrapper on python-foreman package
+Wrapper on Foreman API
+
+## Intro
+Help you to add, update and remove elements from foreman collections
+```
+from foreman_wrapper import ForemanHost
+
+f_h = ForemanHost(
+    host_ip="10.11.12.13",
+    host_root_password="test",
+    foreman_url=...,
+    foreman_user=...,
+    foreman_password=...
+)
+f_h.add(build_parameter)
+f_h.build()
+f_h.run_command(["reboot"])
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), fname,
+            )
+        )
+    ).read()
+
+setup(
+    name="foreman-wrapper",
+    version="0.1",
+    author="Lukianov Artyom",
+    author_email="artyom.lukianov@gmail.com",
+    description="Foreman API wrapper",
+    license="GPL2",
+    keywords="foreman",
+    url="https://github.com/cynepco3hahue/python-foreman-wrapper",
+    platforms=["linux"],
+    packages=["foreman-wrapper"],
+    long_description=read("README.md"),
+    install_requires=["python-foreman", "python-rrmngmnt"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License 2 (GPL2)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+    ],
+)
